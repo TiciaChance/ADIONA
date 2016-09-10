26720d31360bcfeba450fbc8c27f5169ddc8efe2
sheafk <sheafureyking@gmail.com>
2016-09-10 15:30:26 -0400

    Added Adiona Color Extensions

---
+++ ADIONA/UIColor+Extensions.swift
@@ -0,0 +1,53 @@
+//
+//  UIColor+Extensions.swift
+//  ADIONA
+//
+//  Created by Flatiron School on 9/10/16.
+//  Copyright © 2016 Laticia Chance. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+    
+    class func fromRgbHex(fromHex: Int) -> UIColor {
+        
+        let red =   CGFloat((fromHex & 0xFF0000) >> 16) / 0xFF
+        let green = CGFloat((fromHex & 0x00FF00) >> 8) / 0xFF
+        let blue =  CGFloat(fromHex & 0x0000FF) / 0xFF
+        let alpha = CGFloat(1.0)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    class func adionaPurple() -> UIColor {
+        
+        let deepPurple = 0x87255B
+        return UIColor.fromRgbHex(deepPurple)
+    }
+    
+    class func adionaSecondaryPurple() -> UIColor {
+        
+        let mediumPurple = 0xB370B0
+        return UIColor.fromRgbHex(mediumPurple)
+    }
+    
+    class func adionaLavender() -> UIColor {
+        
+        let lavender = 0xD1C8E1
+        return UIColor.fromRgbHex(lavender)
+    }
+    
+    class func adionaLilac() -> UIColor {
+        
+        let lilac = 0xBBA0CA
+        return UIColor.fromRgbHex(lilac)
+    }
+    
+    class func adionaGrayPurple() -> UIColor {
+        
+        let grayPurple = 0xC3C3E6
+        return UIColor.fromRgbHex(grayPurple)
+    }
+    
+}
