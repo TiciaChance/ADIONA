3e303b9bcfc6ea0dcf753cf82f8bc0372273c16c
Janet Zhang <janetzhang321@gmail.com>
2016-09-10 15:46:50 -0400

    testing mess

--- ADIONA/ViewController.swift
+++ ADIONA/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import MessageUI
 
 class ViewController: UIViewController {
 
@@ -21,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Create a MessageComposer
+    let messageComposer = MessageComposer()
+    
+    @IBAction func sendTextMessageButtonTapped(sender: UIButton) {
+        // Make sure the device can send text messages
+        if (messageComposer.canSendText()) {
+            // Obtain a configured MFMessageComposeViewController
+            let messageComposeVC = messageComposer.configuredMessageComposeViewController()
+            
+            // Present the configured MFMessageComposeViewController instance
+            // Note that the dismissal of the VC will be handled by the messageComposer instance,
+            // since it implements the appropriate delegate call-back
+            presentViewController(messageComposeVC, animated: true, completion: nil)
+        } else {
+            // Let the user know if his/her device isn't able to send text messages
+            let errorAlert = UIAlertController(
+                title: "Cannot Send Text Message",
+                message: "Your device is not able to send text messages.",
+                preferredStyle: .Alert)
+            errorAlert.actions
+        }
+    }
+
+    
     
     
 
